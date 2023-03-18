39312d125df5789673d430a72ca27acd0e8f7991
Hoang-TamSE <kakasika15@gmail.com>
2023-03-19 00:10:37 +0700

    update mess

--- src/App_2.js
+++ src/App_2.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   MDBContainer,
   MDBRow,
@@ -16,16 +16,49 @@ import PerfectScrollbar from 'react-perfect-scrollbar';
 
 export default function App_2() {
   const [file, setFile] = useState(null);
+  const [urlImage, setUrlImage] = useState(null);
+  const [newMessage, setNewMessage] = useState('');
+  const [userMesss, setUserMesss] = useState(['aaaaaaaaaaaaaaaa', 'aaaaaaaaaaaaaaaaaaaaaaaaa']);
+  const onSubmitMess = (e) => {
+    setUrlImage(arr => [...arr, e.value.target])
+  }
+
   const [jd, setjd] = useState("");
   const onJDChange = (e) => {
     setjd(e.target.value)
   }
+  var indents = userMesss.map(function (i) {
+    return (
+      <div className="d-flex flex-row justify-content-end">
+        <div>
+          <p className="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">
+            {i}
+          </p>
+
+        </div>
+        <img
+          src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
+          alt="avatar 1"
+          style={{ width: "45px", height: "100%" }}
+        />
+      </div>
+    );
+  });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (newMessage !== '') {
+      setUserMesss([...userMesss, newMessage]);
+      setNewMessage('');
+    }
+  };
   const onFileChange = (e) => {
-    if(e.target.files){
-      if(e.target.files[0].type === "application/pdf" || e.target.files[0].type === "image/png" )
-        setFile(e.target.files[0])
-      else
-        return
+    if (e.target.files) {
+      // if(e.target.files[0].type === "application/pdf" || e.target.files[0].type === "image/png" )
+      //   setFile(e.target.files[0])
+      // else
+      //   return
+      setFile(e.target.files[0])
+
     }
   }
   const toBase64 = file => new Promise((resolve, reject) => {
@@ -33,24 +66,26 @@ export default function App_2() {
     reader.readAsDataURL(file);
     reader.onload = () => resolve(reader.result);
     reader.onerror = error => reject(error);
-});
-  const [viewPdf, setViewPdf]=useState(null);
+  });
+  const [viewPdf, setViewPdf] = useState(null);
 
   const handleUploadClick = async () => {
     if (!file) {
       return;
     }
+    setUrlImage(URL.createObjectURL(file))
     const cvBase64 = await toBase64(file);
     const base64ImageData = cvBase64.substring(cvBase64.indexOf(',') + 1);
-    console.log(JSON.stringify({ 
-       jd,
-       base64ImageData }))
+    console.log(JSON.stringify({
+      jd,
+      base64ImageData
+    }))
 
 
     // 👇 Uploading the file using the fetch API to the server
-    fetch('https://flat-insects-boil-103-160-76-63.loca.lt/predict', {
+    fetch('http://103.160.76.63:5000/predict', {
       method: 'POST',
-      body: JSON.stringify({ jd: {jd}, cv: {base64ImageData} }),
+      body: JSON.stringify({ jd: { jd }, cv: { base64ImageData } }),
       // 👇 Set headers manually for single file upload
       headers: {
         'content-type': file.type,
@@ -71,13 +106,13 @@ export default function App_2() {
                 <MDBCol md="4" lg="4" xl="4" className="mb-4 mb-md-0">
                   <input type="file" onChange={onFileChange} />
                   <div>{file && `${file.name} - ${file.type}`}</div>
-                  <div style={{margin: "10px"}}><p>Job Description</p></div>
-                  <textarea  style={{width: "350px", height: "450px"}}  placeholder="Input JD..." onChange={onJDChange} />
+                  <div style={{ margin: "10px" }}><p>Job Description</p></div>
+                  <textarea style={{ width: "350px", height: "450px" }} placeholder="Input JD..." onChange={onJDChange} />
                   <br></br>
-                  <input type="button" value="Submit" className="btn btn-success btn-lg" onClick={handleUploadClick}/>
+                  <input type="button" value="Submit" className="btn btn-success btn-lg" onClick={handleUploadClick} />
                 </MDBCol>
                 <MDBCol md="4" lg="4" xl="4" className="mb-4 mb-md-0 pdf-container">
-                  
+                  <img src={urlImage} />
                 </MDBCol>
                 <MDBCol md="4" lg="4" xl="4">
                   <PerfectScrollbar
@@ -100,9 +135,7 @@ export default function App_2() {
                           elit, sed do eiusmod tempor incididunt ut labore et
                           dolore magna aliqua.
                         </p>
-                        <p className="small ms-3 mb-3 rounded-3 text-muted float-end">
-                          12:00 PM | Aug 13
-                        </p>
+
                       </div>
                     </div>
 
@@ -113,9 +146,7 @@ export default function App_2() {
                           ullamco laboris nisi ut aliquip ex ea commodo
                           consequat.
                         </p>
-                        <p className="small me-3 mb-3 rounded-3 text-muted">
-                          12:00 PM | Aug 13
-                        </p>
+
                       </div>
                       <img
                         src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
@@ -123,126 +154,25 @@ export default function App_2() {
                         style={{ width: "45px", height: "100%" }}
                       />
                     </div>
+                    {userMesss.map(function (i) {
+                      return (
+                        <div className="d-flex flex-row justify-content-end">
+                          <div>
+                            <p className="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">
+                              {i}
+                            </p>
 
-                    <div className="d-flex flex-row justify-content-start">
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava6-bg.webp"
-                        alt="avatar 1"
-                        style={{ width: "45px", height: "100%" }}
-                      />
-                      <div>
-                        <p
-                          className="small p-2 ms-3 mb-1 rounded-3"
-                          style={{ backgroundColor: "#f5f6f7" }}
-                        >
-                          Duis aute irure dolor in reprehenderit in voluptate
-                          velit esse cillum dolore eu fugiat nulla pariatur.
-                        </p>
-                        <p className="small ms-3 mb-3 rounded-3 text-muted float-end">
-                          12:00 PM | Aug 13
-                        </p>
-                      </div>
-                    </div>
+                          </div>
+                          <img
+                            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
+                            alt="avatar 1"
+                            style={{ width: "45px", height: "100%" }}
+                          />
+                        </div>
+                      );
+                    })}
 
-                    <div className="d-flex flex-row justify-content-end">
-                      <div>
-                        <p className="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">
-                          Excepteur sint occaecat cupidatat non proident, sunt
-                          in culpa qui officia deserunt mollit anim id est
-                          laborum.
-                        </p>
-                        <p className="small me-3 mb-3 rounded-3 text-muted">
-                          12:00 PM | Aug 13
-                        </p>
-                      </div>
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
-                        alt="avatar 1"
-                        style={{ width: "45px", height: "100%" }}
-                      />
-                    </div>
 
-                    <div className="d-flex flex-row justify-content-start">
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava6-bg.webp"
-                        alt="avatar 1"
-                        style={{ width: "45px", height: "100%" }}
-                      />
-                      <div>
-                        <p
-                          className="small p-2 ms-3 mb-1 rounded-3"
-                          style={{ backgroundColor: "#f5f6f7" }}
-                        >
-                          Sed ut perspiciatis unde omnis iste natus error sit
-                          voluptatem accusantium doloremque laudantium, totam
-                          rem aperiam, eaque ipsa quae ab illo inventore
-                          veritatis et quasi architecto beatae vitae dicta sunt
-                          explicabo.
-                        </p>
-                        <p className="small ms-3 mb-3 rounded-3 text-muted float-end">
-                          12:00 PM | Aug 13
-                        </p>
-                      </div>
-                    </div>
-
-                    <div className="d-flex flex-row justify-content-end">
-                      <div>
-                        <p className="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">
-                          Nemo enim ipsam voluptatem quia voluptas sit
-                          aspernatur aut odit aut fugit, sed quia consequuntur
-                          magni dolores eos qui ratione voluptatem sequi
-                          nesciunt.
-                        </p>
-                        <p className="small me-3 mb-3 rounded-3 text-muted">
-                          12:00 PM | Aug 13
-                        </p>
-                      </div>
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
-                        alt="avatar 1"
-                        style={{ width: "45px", height: "100%" }}
-                      />
-                    </div>
-
-                    <div className="d-flex flex-row justify-content-start">
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava6-bg.webp"
-                        alt="avatar 1"
-                        style={{ width: "45px", height: "100%" }}
-                      />
-                      <div>
-                        <p
-                          className="small p-2 ms-3 mb-1 rounded-3"
-                          style={{ backgroundColor: "#f5f6f7" }}
-                        >
-                          Neque porro quisquam est, qui dolorem ipsum quia dolor
-                          sit amet, consectetur, adipisci velit, sed quia non
-                          numquam eius modi tempora incidunt ut labore et dolore
-                          magnam aliquam quaerat voluptatem.
-                        </p>
-                        <p className="small ms-3 mb-3 rounded-3 text-muted float-end">
-                          12:00 PM | Aug 13
-                        </p>
-                      </div>
-                    </div>
-
-                    <div className="d-flex flex-row justify-content-end">
-                      <div>
-                        <p className="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">
-                          Ut enim ad minima veniam, quis nostrum exercitationem
-                          ullam corporis suscipit laboriosam, nisi ut aliquid ex
-                          ea commodi consequatur?
-                        </p>
-                        <p className="small me-3 mb-3 rounded-3 text-muted">
-                          12:00 PM | Aug 13
-                        </p>
-                      </div>
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
-                        alt="avatar 1"
-                        style={{ width: "45px", height: "100%" }}
-                      />
-                    </div>
                   </PerfectScrollbar>
                   <div className="text-muted d-flex justify-content-start align-items-center pe-3 pt-3 mt-2">
                     <img
@@ -255,6 +185,8 @@ export default function App_2() {
                       className="form-control form-control-lg"
                       id="exampleFormControlInput2"
                       placeholder="Type message"
+                      value={newMessage}
+                      onChange={(event) => setNewMessage(event.target.value)}
                     />
                     <a className="ms-1 text-muted" href="#!">
                       <MDBIcon fas icon="paperclip" />
@@ -262,7 +194,7 @@ export default function App_2() {
                     <a className="ms-3 text-muted" href="#!">
                       <MDBIcon fas icon="smile" />
                     </a>
-                    <a className="ms-3" href="#!">
+                    <a className="ms-3" href="/" onClick={handleSubmit}>
                       <MDBIcon fas icon="paper-plane" />
                     </a>
                   </div>
