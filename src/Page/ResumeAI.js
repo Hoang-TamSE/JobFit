a50ca83977512f98348f8d700ac7308e62d0b987
anproa31 <antoeta31@gmail.com>
2023-03-19 03:48:47 +0700

    UI

---
+++ src/Page/ResumeAI.js
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardHeader,
+  MDBCardBody,
+  MDBIcon,
+  MDBBtn,
+  MDBCardFooter,
+  MDBInputGroup,
+} from "mdb-react-ui-kit";
+
+import "react-perfect-scrollbar/dist/css/styles.css";
+
+import PerfectScrollbar from "react-perfect-scrollbar";
+
+export default function ResumeAI() {
+  const [message, setMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  const onInputChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (message !== "") {
+      setMessageList([...messageList, message]);
+      setMessage("");
+    }
+  };
+
+  return (
+    <div>
+      <MDBContainer fluid className="py-5" style={{ backgroundColor: "#FFF" }}>
+        <MDBRow className="d-flex justify-content-right">
+          <MDBCol md="8" lg="6" xl="4">
+            <MDBCard>
+              <MDBCardHeader
+                className="d-flex justify-content-between align-items-center p-3"
+                style={{ borderTop: "4px solid #ffa900" }}
+              >
+                <h5 className="mb-0">Chat messages</h5>
+                <div className="d-flex flex-row align-items-center">
+                  <span className="badge bg-warning me-3">20</span>
+                  <MDBIcon
+                    fas
+                    icon="minus"
+                    size="xs"
+                    className="me-3 text-muted"
+                  />
+                  <MDBIcon
+                    fas
+                    icon="comments"
+                    size="xs"
+                    className="me-3 text-muted"
+                  />
+                  <MDBIcon
+                    fas
+                    icon="times"
+                    size="xs"
+                    className="me-3 text-muted"
+                  />
+                </div>
+              </MDBCardHeader>
+              <PerfectScrollbar
+                style={{ position: "relative", height: "400px" }}
+              >
+                <MDBCardBody>
+                  {messageList.map(function(i) {
+                    return (
+                      <div className="d-flex flex-row justify-content-end">
+                        <div>
+                          <p className="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">
+                            {i}
+                          </p>
+                        </div>
+                        <img
+                          src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
+                          alt="avatar 1"
+                          style={{ width: "45px", height: "100%" }}
+                        />
+                      </div>
+                    );
+                  })}
+                </MDBCardBody>
+              </PerfectScrollbar>
+              <MDBCardFooter className="text-muted d-flex justify-content-start align-items-center p-3">
+                <MDBInputGroup className="mb-0">
+                  <input
+                    className="form-control"
+                    placeholder="Type message"
+                    type="text"
+                    value={message}
+                    onChange={onInputChange}
+                  />
+                  <MDBBtn
+                    color="warning"
+                    style={{ paddingTop: ".55rem" }}
+                    onClick={handleSubmit}
+                  >
+                    Send
+                  </MDBBtn>
+                </MDBInputGroup>
+              </MDBCardFooter>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import './index.css';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import reportWebVitals from "./reportWebVitals";
+import App from "./App";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
